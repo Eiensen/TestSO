046fc6c4a1030ba0e0cca1bc3b43bf34d3cc2339
Minayev Aleksandr <tenshisasha@gmail.com>
2020-11-19 22:31:27 +0300

    Добавил репозиторий

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour
+{
+    public HeroCardSO[] heroCard = new HeroCardSO[5];
+    public TextMeshProUGUI nameCard;
+    public TextMeshProUGUI lvl;
+    public TextMeshProUGUI description;
+    public TextMeshProUGUI defenceValue;
+    public TextMeshProUGUI attackValue;
+    public TextMeshProUGUI attackMode;
+    public TextMeshProUGUI grade;
+    public TextMeshProUGUI kingdom;
+    public GameObject border;   
+   
+
+    public void StartButton()
+    {
+        if (!gameObject.activeInHierarchy)
+        {
+            gameObject.SetActive(true);
+        }
+        HeroCardSO heroCardTmp = heroCard[Random.Range(0, 4)];
+        SetBorderColor(heroCardTmp);
+        SetCardInfo(heroCardTmp);
+    }
+
+    private void SetBorderColor(HeroCardSO heroCard)
+    {       
+        switch (heroCard.warriorGrade)
+        {
+            case HeroCardSO.XColor.grey:
+                border.GetComponent<Image>().color = Color.gray;
+                break;
+            case HeroCardSO.XColor.green:
+                border.GetComponent<Image>().color = Color.green;
+                break;
+            case HeroCardSO.XColor.blue:
+                border.GetComponent<Image>().color = Color.blue;
+                break;          
+        }
+    }
+   private void SetCardInfo(HeroCardSO heroCard)
+    {
+        nameCard.text = heroCard.name;
+        lvl.text = heroCard.level.ToString();
+        description.text = heroCard.description;
+        defenceValue.text = (heroCard.GetDefenceFactor() * 100).ToString();
+        attackValue.text = (heroCard.GetAttackFactor() * 150).ToString();
+        attackMode.text = heroCard.attackType.ToString();
+        kingdom.text = "Королевство: " + heroCard.heroRace;
+        switch (heroCard.warriorGrade)
+        {
+            case HeroCardSO.XColor.grey:
+                grade.text = "x1";
+                grade.color = Color.gray;
+                break;
+            case HeroCardSO.XColor.green:
+                grade.text = "x2";
+                grade.color = Color.green;
+                break;
+            case HeroCardSO.XColor.blue:
+                grade.text = "x3";
+                grade.color = Color.blue;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/HeroCardSO.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+
+public class HeroCardSO : ScriptableObject
+{
+    public enum XColor
+    {
+        grey, green, blue
+    }
+    public enum battle
+    {
+        melee, range
+    }
+    public enum kingdoms
+    {
+        Gnome = 1, Human, Elf
+    }
+
+    public string ID; //Для хранения в базе данных
+
+    public new string name; // Имя персонажа
+
+    public XColor warriorGrade; // Грейд персонажа
+
+    public int level; // Уровень прокачки персонажа
+
+    public battle attackType; // Ближний или дальний бой
+
+    public kingdoms heroRace;
+
+    //public Sprite avatar; // Изображение персонажа
+
+    float levelingFactor;
+    float attackModeFactor;
+    float defenceModeFactor;
+
+    public Dictionary<int, float> raceFactorAttack = new Dictionary<int, float>
+    {
+        [1] = .0f,
+        [2] = .0f,
+        [3] = .0f
+    }; // Именованный массив со значениеями по атаке для каждого королевства
+        
+    public Dictionary<int, float> raceFactorDefence = new Dictionary<int, float>
+    {
+        [1] = .0f,
+        [2] = .0f,
+        [3] = .0f
+    }; // Именованный массив со значениеями по защите для каждого королевства
+
+
+    [Tooltip("Описание")]
+
+    public string description;
+
+    private float GetLevelingFactor()
+    {       
+        switch (warriorGrade)
+        {
+            case XColor.grey:
+               levelingFactor = 0.01f;
+                break;
+            case XColor.green:
+               levelingFactor = 0.02f;
+                break;
+            case XColor.blue:
+              levelingFactor = 0.03f;
+                break;
+        }
+        return levelingFactor;
+    }
+
+    private float GetAttackModeFactor()
+    {
+        switch (attackType)
+        {
+            case battle.melee:
+                attackModeFactor = 1.0f;
+                break;
+            case battle.range:
+                attackModeFactor = 1.5f;
+                break;            
+        }
+        return attackModeFactor;
+    }
+
+    private float GetDefenceModeFactor()
+    {
+        switch (attackType)
+        {
+            case battle.melee:
+                defenceModeFactor = 1.3f;
+                break;
+            case battle.range:
+                defenceModeFactor = 0.7f;
+                break;          
+        }
+        return defenceModeFactor;
+    }
+ 
+    public float GetAttackFactor()
+    {
+        float result = GetAttackModeFactor() + raceFactorAttack[(int)heroRace] + (GetLevelingFactor() * level);
+
+        return result;
+    }
+  
+    public float GetDefenceFactor()
+    {
+        float result = GetDefenceModeFactor() + raceFactorDefence[(int)heroRace] + (GetLevelingFactor() * level);
+
+        return result;
+    }
+}
