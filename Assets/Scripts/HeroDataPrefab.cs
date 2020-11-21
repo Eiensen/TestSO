3ddf717d8c31f3894d446ba32bbc84f740754dc2
Minayev Aleksandr <tenshisasha@gmail.com>
2020-11-21 22:45:08 +0300

    Рабочая тестовая стадия

---
+++ Assets/Scripts/HeroDataPrefab.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HeroDataPrefab : MonoBehaviour, IPointerDownHandler
+{
+    private HeroCardSO heroData;
+    private CardView cardViewScript;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        cardViewScript = FindObjectOfType<CardView>();
+    }
+
+    public void SetHeroData (HeroCardSO hero)
+    {
+        heroData = hero;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        cardViewScript.ViewFromInventory(heroData);
+    }
+}
